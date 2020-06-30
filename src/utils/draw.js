39f0666d826a9940e2ae94ee944af42d17fbaff6
João Nuno Mota <jnuno.carrilho@gmail.com>
2020-06-30 16:01:05 +0100

    Change const function to function on draw utlis

--- src/utils/draw.js
+++ src/utils/draw.js
@@ -1,17 +1,15 @@
-const circle = function(context, actor) {
+export function circle(context, actor) {
   context.fillStyle = actor.color;
   context.beginPath();
   context.arc(actor.x, actor.y, actor.size / 2, 0, Math.PI * 2);
   context.fill();
-};
+}
 
-const square = function(context, actor) {
+export function square(context, actor) {
   context.fillStyle = actor.color;
   context.fillRect(actor.x, actor.y, actor.size, actor.size);
-};
+}
 
-const clear = function(context) {
+export function clear(context) {
   context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-};
-
-export { circle, clear, square };
+}
